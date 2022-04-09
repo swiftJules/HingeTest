ed817ed33f4cdf5aab11c537979d6a4c85ec6dee
Juliana Connors <juliana.connors.dev@gmail.com>
2022-04-09 18:30:37 -0400

    Initial Commit

---
+++ HingeTest/HingeTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  HingeTestApp.swift
+//  HingeTest
+//
+//  Created by RBS70 on 4/9/22.
+//
+
+import SwiftUI
+
+@main
+struct HingeTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
