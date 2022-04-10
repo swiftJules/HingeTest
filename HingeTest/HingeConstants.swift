06b60ef6fa1eff037804921cc52c846276b1f950
Juliana Connors <juliana.connors.dev@gmail.com>
2022-04-09 21:14:44 -0400

    create model, view model and network files

---
+++ HingeTest/HingeConstants.swift
@@ -0,0 +1,8 @@
+//
+//  HingeConstants.swift
+//  HingeTest
+//
+//  Created by RBS70 on 4/9/22.
+//
+
+import Foundation
---
+++ HingeTest/Network/NetworkRequester.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkRequester.swift
+//  HingeTest
+//
+//  Created by RBS70 on 4/9/22.
+//
+
+import Foundation
