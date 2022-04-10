9700b408be2dd0476d1e3460b27cdfc88211e423
Juliana Connors <juliana.connors.dev@gmail.com>
2022-04-09 21:18:46 -0400

    create networking class to handle fetch calls

--- HingeTest/Network/NetworkRequester.swift
+++ HingeTest/Network/NetworkRequester.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+class NetworkRequester {
+    static let shared = NetworkRequester()
+    
+    func fetchUsers(closure: @escaping ([UserViewModel]) -> Void) {
+        guard let url = URL(string: HingeConstants.userURL) else { return }
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data else { return }
+            do {
+                let decoder = JSONDecoder()
+                let response = try decoder.decode(Users.self, from: data)
+                 var userViewModels: [UserViewModel] = []
+                for userModel in response.users {
+                    let viewModel = UserViewModel(model: userModel)
+                    userViewModels.append(viewModel)
+                }
+                closure(userViewModels)
+            } catch {
+                print(error)
+            }
+        }.resume()
+    }
+}
