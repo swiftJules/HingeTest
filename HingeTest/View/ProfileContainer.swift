5d00c02e761d131c17d0086e7d602f3e87cd675c
Juliana Connors <juliana.connors.dev@gmail.com>
2022-04-10 08:53:52 -0400

    add logic to ensure index is not out of range

--- HingeTest/View/ProfileContainer.swift
+++ HingeTest/View/ProfileContainer.swift
@@ -9,13 +9,27 @@ import SwiftUI
 
 struct ProfileContainer: View {
     let usersViewModel: UsersViewModel?
-    let index = 0
+    @State var index = 0
     var body: some View {
-        VStack(alignment: .leading) {
-            NameView(name: usersViewModel?.users.first?.name ?? "")
-                .padding([.bottom, .leading])
-            ProfileImage(urlString: usersViewModel?.users.first?.name ?? "")
-            Spacer()
-        }//:VSTACK
+        if index < usersViewModel?.users.count ?? 0 {
+            ZStack {
+                VStack(alignment: .leading) {
+                    NameView(name: usersViewModel?.users[index].name ?? "")
+                        .padding([.bottom, .leading])
+                    if let photoString = usersViewModel?.users[index].photo {
+                        ProfileImage(urlString: photoString)
+                    }
+                    DetailView(user: usersViewModel?.users[index])
+                    Spacer()
+                }//:VSTACK
+                ZStack {
+                    Button("Next") {
+                        index += 1
+                    }
+                    .frame(maxHeight: .infinity, alignment: .bottom)
+                    .padding([.bottom, .leading])
+                }
+            }//:ZSTACK
+        }
     }
 }
