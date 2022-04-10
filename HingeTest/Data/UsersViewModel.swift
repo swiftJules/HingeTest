aa73eccb3c7ba8b5e37b3073a310751c40be83e5
Juliana Connors <juliana.connors.dev@gmail.com>
2022-04-09 21:29:09 -0400

    add func to call config fetch on network class

--- HingeTest/Data/UsersViewModel.swift
+++ HingeTest/Data/UsersViewModel.swift
@@ -9,14 +9,22 @@ import Foundation
 
 class UsersViewModel {
     var users: [UserViewModel] = []
+    var config: [String] = []
     
     init() {
         self.fetchUsers()
+        self.fetchConfig()
     }
     
     func fetchUsers() {
         NetworkRequester.shared.fetchUsers { users in
             self.users = users
+        }
+    }
+    
+    func fetchConfig() {
+        NetworkRequester.shared.fetchConfig { config in
+            self.config = config.profile
         }
     }
 }
