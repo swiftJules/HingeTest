60f4771c55d1f7e3f6aa717ff66e9ee00372db40
Juliana Connors <juliana.connors.dev@gmail.com>
2022-04-09 21:16:42 -0400

    create view model class with user details

--- HingeTest/Data/UserViewModel.swift
+++ HingeTest/Data/UserViewModel.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+
+class UserViewModel {
+    let model: User?
+    
+    init(model: User) {
+        self.model = model
+    }
+    
+    var name: String {
+        model?.name ?? ""
+    }
+    
+    var photo: String {
+        model?.photo ?? ""
+    }
+    
+    var gender: String {
+        if model?.gender == "f" {
+            return "female"
+        } else {
+            return "male"
+        }
+    }
+    
+    var about: String {
+        model?.about ?? ""
+    }
+    
+    var school: String {
+        model?.school ?? ""
+    }
+    
+    var hobbies: String {
+        model?.hobbies?.first ?? ""
+    }
+}
