5621ad0bffe8c47dc06bcf1dd5ebe24517f63220
Juliana Connors <juliana.connors.dev@gmail.com>
2022-04-09 21:39:54 -0400

    add name component

--- HingeTest/View/NameView.swift
+++ HingeTest/View/NameView.swift
@@ -8,13 +8,10 @@
 import SwiftUI
 
 struct NameView: View {
+    let name: String
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-struct NameView_Previews: PreviewProvider {
-    static var previews: some View {
-        NameView()
+        Text(name)
+            .font(.title)
+            .bold()
     }
 }
