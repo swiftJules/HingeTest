8270f203c91240c58687b4e2e459f331ba740cdf
Juliana Connors <juliana.connors.dev@gmail.com>
2022-04-10 08:51:29 -0400

    add conditional to unwrap image url

--- HingeTest/View/ProfileImage.swift
+++ HingeTest/View/ProfileImage.swift
@@ -10,18 +10,19 @@ import SwiftUI
 struct ProfileImage: View {
     let urlString: String
     var body: some View {
-        let url = URL(string: urlString)!
-        AsyncImage(url: url) { image in
-            image
-                .resizable()
-                .aspectRatio(contentMode: .fill)
-                .frame(height: 400)
-
-                .clipped()
-        } placeholder: {
-            Image(systemName: "photo")
-                .imageScale(.small)
-                .foregroundColor(.gray)
+        if let url = URL(string: urlString) {
+            AsyncImage(url: url) { image in
+                image
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(height: 400)
+                
+                    .clipped()
+            } placeholder: {
+                Image(systemName: "photo")
+                    .imageScale(.small)
+                    .foregroundColor(.gray)
+            }
         }
     }
 }
