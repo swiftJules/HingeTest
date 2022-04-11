e5b44fc0f8004363f6180b938040cbf6a0d98ba2
Juliana Connors <juliana.connors.dev@gmail.com>
2022-04-11 09:34:08 -0400

    rename to request view model

--- HingeTest/Data/RequestViewModel.swift
+++ HingeTest/Data/RequestViewModel.swift
@@ -6,3 +6,37 @@
 //
 
 import Foundation
+
+class RequestViewModel {
+    var users: [UserViewModel] = []
+    var sortedViews: [Any] = []
+    var configuration: [String] = []
+    
+    init() {
+        self.fetchUsers()
+        self.fetchConfig()
+    }
+    
+    func fetchUsers() {
+        NetworkRequester.shared.fetchUsers { users in
+            self.users = users
+        }
+    }
+    
+    func fetchConfig() {
+        NetworkRequester.shared.fetchConfig { config in
+            self.configuration = config.profile
+            self.sortViews()
+        }
+    }
+    
+    func sortViews() {
+        let views: [Any] = [About.self, Photo.self, Name.self, Hobbies.self, School.self, Gender.self]
+        
+        for string in configuration {
+            let view = views.filter { view in
+                string == String(describing: view.self).lowercased() }
+            sortedViews.append(view[0])
+        }
+    }
+}
